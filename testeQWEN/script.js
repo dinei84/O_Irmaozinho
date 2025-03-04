13d9c126a7cce5fa65c9eaac705465324449be3d
dinei84 <devdinei@gmail.com>
2025-03-04 17:10:18 -0300

    Ajustado componentização

--- testeQWEN/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menu = document.querySelector(".menu");
-    const logo = document.querySelector(".logo");
-
-    logo.addEventListener("click", function () {
-        menu.classList.toggle("ativo");
-    });
-});
